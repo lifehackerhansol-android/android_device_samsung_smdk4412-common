36afe937a150676654060d7915e4d75fcd64ec2f
fullgreen <abin030187@naver.com>
2017-02-03 23:55:34 +0900

    smdk4412-common: add spn-conf
    G930SKSU1DQA5
    Signed-off-by: fullgreen <abin030187@naver.com>

--- common.mk
+++ common.mk
@@ -98,6 +98,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
